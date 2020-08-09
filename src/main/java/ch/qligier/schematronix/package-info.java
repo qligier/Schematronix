8a1a86d4c9586746a271bd561c19a0e4703ae19f
Quentin Ligier <quentin.ligier@bluewin.ch>
2020-08-09 21:50:52 +0200

    Add package-info file

---
+++ src/main/java/ch/qligier/schematronix/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * A Schematron validator inspired by the pure version of <a href="https://github.com/phax/ph-schematron">ph-schematron</a>, improved to
+ * efficiently validate CDA-CH-EMED documents.
+ *
+ * @author Quentin Ligier
+ * @version 0.3.0-SNAPSHOT
+ */
+package ch.qligier.schematronix;
